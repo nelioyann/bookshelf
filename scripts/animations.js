a1adbbc077614e1f51c04fc22a152a5e6fa52e47
nelioyann <nanayannick1998@gmail.com>
2020-07-31 21:58:24 +0200

    version 1

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,11 +1,4 @@
-gsap.set(".auth__signin", {
-  x: 0,
-  autoAlpha: 1,
-});
-gsap.set(".auth__signup", {
-  x: "100vw",
-  autoAlpha: 0,
-});
+
 
 const toggleTween = tween =>{
     tween.reversed() ? tween.play() : tween.reversed()
@@ -29,5 +22,13 @@ const signup_tl = gsap.timeline({paused:true, reversed: true }).to(".auth__signu
 //   duration: 1
 // })
 
+const showBooks_tl = gsap
+.timeline({paused:true, reversed: true })
+.to(".books__list", { overflow: "none", duration: 0.1 })
+.to(".books__list", { autoAlpha: 0, duration: 0.3 })
+.to(".books__list", { display: "none", duration: 0.3 })
+.to(".books__form", {autoAlpha: 0, duration: 0.1})
+.to(".books__form", { display: "block", duration: 0.1 })
+.to(".books__form", {autoAlpha: 1, duration: 0.3})
 // signin_tl.play()
 // signup_tl.play()
--- scripts/auth.js
+++ scripts/auth.js
@@ -3,21 +3,22 @@ document.addEventListener("DOMContentLoaded", () => {
     if (user) {
       // account info
       // const html = `<div> Logged in as ${user.email}</div>`
-      db.collection("users").doc(user.uid).get().then(doc =>{
-        lg("fetching user doc")
-        // lg(doc)
-        // doc.collection("books").get().then(snapshot =>{
-        //   lg(snapshot)
-        // })
-        $(".header__title").innerHTML = `Logged in as ${doc.data().username}`;
-        // $(".header__title").innerHTML = `Logged in as ${user.email}`;
-      }
-      )
-      
+      db.collection("users")
+        .doc(user.uid)
+        .get()
+        .then((doc) => {
+          lg("fetching user doc");
+          // lg(doc)
+          // doc.collection("books").get().then(snapshot =>{
+          //   lg(snapshot)
+          // })
+          $(".header__title").innerHTML = `Logged in as ${doc.data().username}`;
+          // $(".header__title").innerHTML = `Logged in as ${user.email}`;
+        });
+
       // db.collection("users").doc(user.uid).collection("books").get().then(snapshot =>{
       //   lg(snapshot.docs)
       // })
-      
 
       //toggle UI elements
       $$(".logged-in").forEach((item) => (item.style.display = "block"));
@@ -40,68 +41,95 @@ document.addEventListener("DOMContentLoaded", () => {
     signup_tl.reverse();
   };
 
+// const showBookForm = (e) =>{
+//   gsap
+//         .timeline()
+//         .to(".books__list", { autoAlpha: 0, duration: 0.3 })
+//         .to(".books__list", { display: "none", duration: 0.3 })
+//         .to(".books__form", {autoAlpha: 0, duration: 0.1})
+//         .to(".books__form", { display: "block", duration: 0.1 })
+//         .to(".books__form", {autoAlpha: 1, duration: 0.3})
+//         // .to(".auth", { display: "block", duration: 0.1 });
 
-const setupBooks = (data) => {
-  lg(data)
-  if (data.length) {
-    let html = `
-    <li class="book">
-					<div class="book__title">+</div>
-					<div class="book__completion">Add a new book</div>
-				</li>
+// }
+  const setupBooks = (data) => {
+    lg(data);
+    if (data.length) {
+      let html = `
+    
         `;
-    data.forEach((doc) => {
-      const book = doc.data();
-      const li = `
+      data.forEach((doc) => {
+        const book = doc.data();
+        const li = `
 				<li class="book">
 					<div class="book__title">${book.title}</div>
-					<div class="book__completion">${book.completed ? "finished": "ongoing"}</div>
+					<div class="book__completion">${book.completed ? "finished" : "ongoing"}</div>
 				</li>
 			`;
-      html += li;
-    });
-    $(".books__list").innerHTML = html;
-  } else {
-    $(".books__list").innerHTML = `<h5 class="">Login to see guides</h5>`;
-  }
-};
+        html += li;
+      });
+      $(".books__list").innerHTML += html;
 
+    } else {
+      lg("no books")
+      // $(".books__list").innerHTML = `<h5 class="">You don't have any book</h5>`;
+    }
+  };
 
   // listen for auth status changes
   auth.onAuthStateChanged((user) => {
     if (user) {
+      
       //   account information
       lg("user logged inn");
-      lg(user)
+      lg(user);
       // Hide forms
       hideForms();
 
       // get data
-      db.collection("users").doc(user.uid).collection("books")
-      .onSnapshot((snapshot) => {
-        setupBooks(snapshot.docs);
-        setupUI(user);
-        
-        // Show books
-        $(".auth").style.display = "none"
-        gsap.timeline()
-        .to(".books", {display: "block"})
-        .to(".book", {autoAlpha: 1, duration: 1, delay: 0.3})
-        .to(".books", {overflow: "auto"})
+      db.collection("users")
+        .doc(user.uid)
+        .collection("books")
+        .onSnapshot(
+          (snapshot) => {
+            setupBooks(snapshot.docs);
 
-        },
-        err => {
-          console.log(err.message);
-        })
+      // lg($(".add__book"))
+      
+            setupUI(user);
+
+            // Show books
+            $(".auth").style.display = "none";
+            gsap
+              .timeline()
+              .to(".books", { display: "block" })
+              .to(".book", { autoAlpha: 1, duration: 1, delay: 0.3 })
+              .to(".books", { overflow: "auto" });
+            
+          },
+          (err) => {
+            console.log(err.message);
+          }
+          
+        );
     } else {
+      gsap.set(".auth__signin", {
+        x: 0,
+        autoAlpha: 1,
+      });
+      gsap.set(".auth__signup", {
+        x: "100vw",
+        autoAlpha: 0,
+      });
       console.log("user logged out");
       signin_tl.reverse();
       // signup_tl.reverse(false)
-      gsap.timeline()
-      .to(".books", {overflow:"hidden", duration: 0.1})
-      // .to(".book", {autoAlpha: 0, duration: 0.3})
-      .to(".books", {display:"none", duration: 0.1})
-      .to(".auth", {display:"block", duration: 0.1})
+      gsap
+        .timeline()
+        .to(".books", { overflow: "hidden", duration: 0.1 })
+        // .to(".book", {autoAlpha: 0, duration: 0.3})
+        .to(".books", { display: "none", duration: 0.1 })
+        .to(".auth", { display: "block", duration: 0.1 });
       // $(".auth").style.display = "block"
       setupUI();
       // setupGuides([]);
@@ -109,28 +137,45 @@ const setupBooks = (data) => {
   });
 
   //create book
-    $(".books__form").addEventListener("submit", (e) => {
-      e.preventDefault();
+  $(".link__add").addEventListener("click", ()=>{
+    showBooks_tl.play()
+  })
+  $(".link__books").addEventListener("click", ()=>{
+    showBooks_tl.reverse()
+  })
 
-      db.collection("books")
+  $(".books__form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let user = auth.currentUser;
+    lg(user.uid)
+    // lg()
+    db.collection("users").doc(user.uid).collection("books")
       // add completion
       // add total
-        .add({
-          title: $(".books__form")["title"].value,
-          content: $(".books__form")["progress"].value,
-        })
-        .then(() => {
-          // reset form
+      .add({
+        title: $(".books__form")["title"].value,
+        progress: $(".books__form")["progress"].value,
+        completed: $(".books__form")["completed"].checked
+      })
+      .then(() => {
+        // reset form
 
-          $(".books__form").reset();
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-    });
+        $(".books__form").reset();
+        // $(".auth").style.display = "none";
+        gsap
+          .timeline()
+          .to(".books__form", { autoAlpha: 0, duration: 0.3 })
+          .to(".books__form", { display: "none" })
+          .to(".books__list", { display: "flex", duration: 1, delay: 0.3 })
+          .to(".books__list", { autoAlpha: 1, duration: 1 })
+          // .to(".books", { overflow: "auto" });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
 
-  
-    // signup
+  // signup
   $(".auth__signup").addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -138,18 +183,25 @@ const setupBooks = (data) => {
     const email = $(".auth__signup")["new-email"].value;
     const password = $(".auth__signup")["new-password"].value;
     //   sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-      return db.collection("users").doc(cred.user.uid).set({
-        username: $(".auth__signup")["username"].value
-      })
-      // reset form
-      // $(".auth__signup").reset();
-    }, err => lg(err.message))
-    .then(()=>{
-      lg("reset here")
-      $(".auth__signup").reset();
-
-    })
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(
+        (cred) => {
+          return db
+            .collection("users")
+            .doc(cred.user.uid)
+            .set({
+              username: $(".auth__signup")["username"].value,
+            });
+          // reset form
+          // $(".auth__signup").reset();
+        },
+        (err) => lg(err.message)
+      )
+      .then(() => {
+        lg("reset here");
+        $(".auth__signup").reset();
+      });
   });
 
   // logout
@@ -167,11 +219,14 @@ const setupBooks = (data) => {
     const password = $(".auth__signin")["password"].value;
 
     // log the user in
-    auth.signInWithEmailAndPassword(email, password).then((cred) => {
-      // hide form
-      hideForms();
-      // reset form
-      $(".auth__signin").reset();
-    }).catch(err => lg(err.message))
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((cred) => {
+        // hide form
+        hideForms();
+        // reset form
+        $(".auth__signin").reset();
+      })
+      .catch((err) => lg(err.message));
   });
 });
